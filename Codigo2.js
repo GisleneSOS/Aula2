e922855d9ae27ccfd1bd29ef5e3ebc6470e14172
GisleneSOS <gislenesilva2008@outlook.com>
2024-02-16 22:55:26 -0300

    Create Codigo2.js

---
+++ Codigo2.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome");
+var idade = prompt("Digite sua idade");
+var linguagem = prompt ("Qual linguagem do programação você está estudando?");
+if(nome!=null && idade!=null & linguagem!=null){
+print("Olá " + nome + "," + " você possui " + idade + " anos e está estudando " + linguagem)
+}
