ddbc950c0be530e4bac2a3de3a67ce981b374eb6
GisleneSOS <gislenesilva2008@outlook.com>
2024-02-16 23:26:20 -0300

    Aula2.css

--- Codigo2.js
+++ Codigo2.js
@@ -2,5 +2,5 @@ var nome = prompt("Digite seu nome");
 var idade = prompt("Digite sua idade");
 var linguagem = prompt ("Qual linguagem do programação você está estudando?");
 if(nome!=null && idade!=null & linguagem!=null){
-print("Olá " + nome + "," + " você possui " + idade + " anos e está estudando " + linguagem)
+alert("Olá " + nome + "," + " você possui " + idade + " anos e está estudando " + linguagem)
 }
